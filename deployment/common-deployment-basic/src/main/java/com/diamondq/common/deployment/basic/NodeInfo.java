c294899f2bf8bb442876c966d50004ae62c859c5
Mike Mansell <me@michaelmansell.com>
2016-09-05 18:24:10 -0700

    Initial checkin of a Server Deployment Solution

---
+++ deployment/common-deployment-basic/src/main/java/com/diamondq/common/deployment/basic/NodeInfo.java
@@ -0,0 +1,17 @@
+package com.diamondq.common.deployment.basic;
+
+public class NodeInfo {
+
+	public boolean	gitInstalled							= false;
+
+	public boolean	chefInstalled							= false;
+
+	public boolean	correctVersionOfChefInstalled			= false;
+
+	public boolean	dependencyCookbooksInstalled			= false;
+
+	public boolean	mainCookbookInstalled					= false;
+
+	public boolean	correctVersionOfMainCookbookInstalled	= false;
+
+}
---
+++ deployment/common-deployment-basic/src/main/java/com/diamondq/common/deployment/basic/TestGitClone.java
@@ -0,0 +1,53 @@
+package com.diamondq.common.deployment.basic;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+
+import org.jclouds.scriptbuilder.domain.GitRepoAndRef;
+import org.jclouds.scriptbuilder.domain.OsFamily;
+import org.jclouds.scriptbuilder.domain.StatementList;
+import org.jclouds.scriptbuilder.domain.Statements;
+import org.jclouds.scriptbuilder.statements.git.CloneGitRepo;
+
+public class TestGitClone extends CloneGitRepo {
+
+	public static Builder builder() {
+		return new TestBuilder();
+	}
+
+	public static class TestBuilder extends Builder {
+
+		public TestBuilder() {
+			super();
+		}
+
+		@Override
+		public CloneGitRepo build() {
+			return new TestGitClone(gitRepoAndRef.build(), directory);
+		}
+	}
+
+	protected TestGitClone(GitRepoAndRef pGitRepoAndRef, Optional<String> pDirectory) {
+		super(pGitRepoAndRef, pDirectory);
+	}
+
+	@Override
+	public String render(OsFamily pArg0) {
+		StringBuilder command = new StringBuilder();
+		command.append("git clone -q");
+		if (gitRepoAndRef.getBranch().isPresent())
+			command.append(" -b ").append(gitRepoAndRef.getBranch().get());
+		command.append(' ').append(gitRepoAndRef.getRepository().toASCIIString());
+		if (directory.isPresent())
+			command.append(' ').append(directory.get());
+		command.append("{lf}");
+		command.append("{cd} ").append(directory.or(
+			Iterables.getLast(Splitter.on('/').split(gitRepoAndRef.getRepository().getPath())).replace(".git", "")));
+		if (gitRepoAndRef.getTag().isPresent()) {
+			command.append("{lf}").append("git checkout ").append(gitRepoAndRef.getTag().get());
+		}
+		return new StatementList(Statements.literal("trap '' 1"), Statements.exec(command.toString())).render(pArg0);
+	}
+
+}
