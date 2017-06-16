9443ede3543ee306749fa5da4321e3a74de32228
Mike Mansell <me@michaelmansell.com>
2017-06-16 14:38:14 -0700

    Modified the setValue to support different data types

--- model/common-model-standard/src/main/java/com/diamondq/common/model/generic/GenericProperty.java
+++ model/common-model-standard/src/main/java/com/diamondq/common/model/generic/GenericProperty.java
@@ -3,11 +3,13 @@ package com.diamondq.common.model.generic;
 import com.diamondq.common.model.interfaces.CommonKeywordKeys;
 import com.diamondq.common.model.interfaces.Property;
 import com.diamondq.common.model.interfaces.PropertyDefinition;
+import com.diamondq.common.model.interfaces.PropertyRef;
 import com.diamondq.common.model.interfaces.Script;
 import com.diamondq.common.model.interfaces.Structure;
 import com.diamondq.common.model.interfaces.StructureRef;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 public class GenericProperty<TYPE> implements Property<TYPE> {
@@ -85,6 +87,7 @@ public class GenericProperty<TYPE> implements Property<TYPE> {
 				TYPE defaultResult = (TYPE) (Boolean) Boolean.parseBoolean(defaultValue);
 				return defaultResult;
 			}
+			case Timestamp:
 			case Binary:
 			case Image:
 			case EmbeddedStructureList:
@@ -109,8 +112,115 @@ public class GenericProperty<TYPE> implements Property<TYPE> {
 	/**
 	 * @see com.diamondq.common.model.interfaces.Property#setValue(java.lang.Object)
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public Property<TYPE> setValue(TYPE pValue) {
+
+		/*
+		 * Because the TYPE is a Generic, and can be lost in type-erasure or can be modeled as a <?>, we should see if
+		 * we need to 'convert' the data type before passing it to the constructor
+		 */
+
+		if (pValue != null) {
+			switch (mPropertyDefinition.getType()) {
+			case Binary:
+				break;
+			case Boolean: {
+				if (pValue instanceof Boolean)
+					break;
+				if (pValue instanceof String) {
+					pValue = (TYPE) (Boolean) Boolean.parseBoolean((String) pValue);
+					break;
+				}
+				throw new IllegalArgumentException("A Boolean Property must only be passed a Boolean or a String");
+			}
+			case Decimal: {
+				if (pValue instanceof BigDecimal)
+					break;
+				if (pValue instanceof Short) {
+					pValue = (TYPE) new BigDecimal((Short) pValue);
+					break;
+				}
+				if (pValue instanceof Long) {
+					pValue = (TYPE) BigDecimal.valueOf((Long) pValue);
+					break;
+				}
+				if (pValue instanceof Integer) {
+					pValue = (TYPE) new BigDecimal((Integer) pValue);
+					break;
+				}
+				if (pValue instanceof Float) {
+					pValue = (TYPE) new BigDecimal((Float) pValue);
+					break;
+				}
+				if (pValue instanceof Double) {
+					pValue = (TYPE) BigDecimal.valueOf((Double) pValue);
+					break;
+				}
+				if (pValue instanceof String) {
+					pValue = (TYPE) new BigDecimal((String) pValue);
+					break;
+				}
+				throw new IllegalArgumentException(
+					"A Decimal Property must be passed a BigDecimal, Short, Long, Integer, Float, Double or String");
+			}
+			case EmbeddedStructureList:
+				break;
+			case Image:
+				break;
+			case Integer: {
+				if (pValue instanceof Integer)
+					break;
+				if (pValue instanceof String) {
+					pValue = (TYPE) (Integer) Integer.parseInt((String) pValue);
+					break;
+				}
+				throw new IllegalArgumentException("An Integer Property must be passed an Integer or a String");
+			}
+			case PropertyRef: {
+				if (pValue instanceof PropertyRef)
+					break;
+				if (pValue instanceof String) {
+					pValue = (TYPE) mPropertyDefinition.getScope().getToolkit()
+						.createPropertyRefFromSerialized(mPropertyDefinition.getScope(), (String) pValue);
+					break;
+				}
+				throw new IllegalArgumentException("A PropertyRef Property must be passed a PropertyRef or a String");
+			}
+			case String: {
+				if (pValue instanceof String)
+					break;
+				pValue = (TYPE) pValue.toString();
+				break;
+			}
+			case StructureRef: {
+				if (pValue instanceof String)
+					break;
+				throw new IllegalArgumentException("A StructureRef Property must be passed a String");
+			}
+			case StructureRefList: {
+				if (pValue instanceof List)
+					break;
+				throw new IllegalArgumentException("A StructureRefList Property must be passed a List");
+			}
+			case Timestamp: {
+				if (pValue instanceof Long)
+					break;
+				if (pValue instanceof Integer) {
+					@SuppressWarnings("cast")
+					long asLong = Long.valueOf((long) (Integer) pValue);
+					pValue = (TYPE) (Long) asLong;
+					break;
+				}
+				if (pValue instanceof String) {
+					pValue = (TYPE) Long.valueOf((String) pValue);
+					break;
+				}
+				throw new IllegalArgumentException("A Timestamp Property must be passed a Long, Integer or String");
+			}
+			}
+		}
+
 		return new GenericProperty<TYPE>(mPropertyDefinition, true, pValue);
 	}
 
