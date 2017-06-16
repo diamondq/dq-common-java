5b5c7f99b792e56cef7b2f251598414061ca165e
Mike Mansell <me@michaelmansell.com>
2017-06-16 14:32:19 -0700

    Improved documentation

--- storage/common-storage-kv/src/main/java/com/diamondq/common/storage/kv/IKVTransaction.java
+++ storage/common-storage-kv/src/main/java/com/diamondq/common/storage/kv/IKVTransaction.java
@@ -1,5 +1,7 @@
 package com.diamondq.common.storage.kv;
 
+import com.google.common.collect.Iterators;
+
 import java.util.Iterator;
 
 public interface IKVTransaction {
@@ -19,8 +21,27 @@ public interface IKVTransaction {
 
 	public boolean removeByKey(String pTable, String pKey1, String pKey2);
 
+	/**
+	 * Returns an iterator that returns all the distinct key 1's within the table. NOTE: It is critically important that
+	 * the iterator is fully consumed, otherwise, some implementations may leak heavily (ie. not closing a
+	 * PreparedStatement or ResultSet). Use {@link Iterators#size(Iterator)} or something similar to consume if it isn't
+	 * already consumed by your code.
+	 * 
+	 * @param pTable the table
+	 * @return an iterator of keys
+	 */
 	public Iterator<String> keyIterator(String pTable);
 
+	/**
+	 * Returns an iterator that returns all the distinct key 2's within the table/key1. NOTE: It is critically important
+	 * that the iterator is fully consumed, otherwise, some implementations may leak heavily (ie. not closing a
+	 * PreparedStatement or ResultSet). Use {@link Iterators#size(Iterator)} or something similar to consume if it isn't
+	 * already consumed by your code.
+	 * 
+	 * @param pTable the table
+	 * @param pKey1 the key 1
+	 * @return an iterator of keys
+	 */
 	public Iterator<String> keyIterator2(String pTable, String pKey1);
 
 	public void clear(String pTable);
