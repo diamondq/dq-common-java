0f42771200e6d7d37d8730f03166ce41b33547df
Mike Mansell <me@michaelmansell.com>
2017-08-07 22:56:52 -0700

    Fixed up the test case to comply with the new design

--- reaction/common-reaction-engine/src/test/java/com/diamondq/common/reaction/engine/tests/simple_trigger/OnNewRecord.java
+++ reaction/common-reaction-engine/src/test/java/com/diamondq/common/reaction/engine/tests/simple_trigger/OnNewRecord.java
@@ -3,18 +3,19 @@ package com.diamondq.common.reaction.engine.tests.simple_trigger;
 import com.diamondq.common.reaction.api.Action;
 import com.diamondq.common.reaction.api.ConfigureReaction;
 import com.diamondq.common.reaction.api.JobContext;
+import com.diamondq.common.reaction.api.info.AbstractNoParamsJobInfo;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class OnNewRecord {
+public class OnNewRecord extends AbstractNoParamsJobInfo {
 
 	@ConfigureReaction
 	public void setupJob(JobContext pContext) {
 		// @formatter:off
-		pContext.registerJob(pContext.newJobBuilder().method(this::modifyRecord)
+		pContext.registerJob(pContext.newJobBuilder().method(this::modifyRecord).info(this)
 			.triggerCollection(Record.class).action(Action.INSERT).build()
-			.param(OtherDependent.class).build()
+			.param(OtherDependent.class).isTransient().build()
 		.build());
 		// @formatter:on
 	}
