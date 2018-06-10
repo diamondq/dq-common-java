1c0d579dca887adb78b7614877cf0d87f9ec469c
Mike Mansell <me@michaelmansell.com>
2018-06-09 17:36:27 -0700

    Added basic Map parsing (mainly used for OSGi props parsing)

---
+++ utils/common-utils.parsing/src/main/java/com/diamondq/common/utils/parsing/properties/PropertiesParsing.java
@@ -0,0 +1,85 @@
+package com.diamondq.common.utils.parsing.properties;
+
+import java.util.Map;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class PropertiesParsing {
+
+	public static boolean getNonNullBoolean(Map<String, Object> pProps, String pKey, Boolean pDefault) {
+		Object propObj = pProps.get(pKey);
+		Boolean bool;
+		if (propObj == null)
+			bool = pDefault;
+		else
+			bool = Boolean.parseBoolean(propObj.toString());
+		return bool;
+	}
+
+	public static @Nullable Boolean getNullableBoolean(Map<String, Object> pProps, String pKey) {
+		Object propObj = pProps.get(pKey);
+		@Nullable
+		Boolean bool;
+		if (propObj == null)
+			bool = null;
+		else
+			bool = Boolean.parseBoolean(propObj.toString());
+		return bool;
+	}
+
+	public static String getNonNullString(Map<String, Object> pProps, String pKey, String pDefault) {
+		Object propObj = pProps.get(pKey);
+		String val;
+		if (propObj == null)
+			val = pDefault;
+		else
+			val = propObj.toString();
+		return val;
+	}
+
+	public static @Nullable String getNullableString(Map<String, Object> pProps, String pKey) {
+		Object propObj = pProps.get(pKey);
+		@Nullable
+		String val;
+		if (propObj == null)
+			val = null;
+		else
+			val = propObj.toString();
+		return val;
+	}
+
+	public static int getNonNullInt(Map<String, Object> pProps, String pKey, int pDefault) {
+		Object propObj = pProps.get(pKey);
+		int val;
+		if (propObj == null)
+			val = pDefault;
+		else
+			val = Integer.parseInt(propObj.toString());
+		return val;
+	}
+
+	public static @Nullable Integer getNullableInt(Map<String, Object> pProps, String pKey) {
+		Object propObj = pProps.get(pKey);
+		@Nullable
+		Integer val;
+		if (propObj == null)
+			val = null;
+		else
+			val = Integer.parseInt(propObj.toString());
+		return val;
+	}
+
+	public static String[] getStringArray(Map<String, Object> pProps, String pKey) {
+		Object propObj = pProps.get(pKey);
+		if (propObj == null)
+			return new String[0];
+		String val = propObj.toString();
+		String[] results = val.split(",");
+		return results;
+	}
+
+	public static void removeAll(Map<String, Object> pProps, String... pKeys) {
+		for (String key : pKeys)
+			pProps.remove(key);
+	}
+}
