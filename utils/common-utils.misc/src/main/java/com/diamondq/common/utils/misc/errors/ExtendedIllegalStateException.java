0ceaea21d5819443c38f854db5fcd7afd19b48d7
Mike Mansell <me@michaelmansell.com>
2018-06-20 10:39:29 -0700

    Added the ExtendedIllegalStateException to support I18N enum messages.

---
+++ utils/common-utils.misc/src/main/java/com/diamondq/common/utils/misc/errors/ExtendedIllegalStateException.java
@@ -0,0 +1,66 @@
+package com.diamondq.common.utils.misc.errors;
+
+import com.diamondq.common.utils.misc.i18n.I18N;
+import com.diamondq.common.utils.misc.i18n.MessagesEnum;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class ExtendedIllegalStateException extends IllegalStateException {
+
+	private static final long			serialVersionUID	= 4687502775610417335L;
+
+	protected final MessagesEnum		mCode;
+
+	protected final @Nullable Object[]	mParams;
+
+	public ExtendedIllegalStateException(MessagesEnum pCode, @Nullable Object @Nullable... pParams) {
+		super();
+		mCode = pCode;
+		if (pParams == null)
+			mParams = new String[0];
+		else
+			mParams = Arrays.copyOf(pParams, pParams.length);
+	}
+
+	public ExtendedIllegalStateException(Throwable pCause, MessagesEnum pCode, @Nullable Object @Nullable... pParams) {
+		super(null, pCause);
+		mCode = pCode;
+		if (pParams == null)
+			mParams = new String[0];
+		else
+			mParams = Arrays.copyOf(pParams, pParams.length);
+	}
+
+	public MessagesEnum getCode() {
+		return mCode;
+	}
+
+	/**
+	 * @see java.lang.Throwable#getMessage()
+	 */
+	@Override
+	public String getMessage() {
+		return getLocalizedMessage(I18N.getDefaultLocale());
+	}
+
+	/**
+	 * @see java.lang.Throwable#getLocalizedMessage()
+	 */
+	@Override
+	public String getLocalizedMessage() {
+		return getLocalizedMessage(I18N.getDefaultLocale());
+	}
+
+	/**
+	 * Returns the error message localized for the given locale
+	 * 
+	 * @param pLocale the locale
+	 * @return the message
+	 */
+	public String getLocalizedMessage(Locale pLocale) {
+		return I18N.getFormat(pLocale, mCode, mParams);
+	}
+}
