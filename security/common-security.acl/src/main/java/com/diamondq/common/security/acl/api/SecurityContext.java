5b76d804e53d99e9d6cc5885a9b474eddc4b7799
Mike Mansell <me@michaelmansell.com>
2018-07-03 18:07:04 -0700

    Added the SecurityContext to the security.acl project.

---
+++ security/common-security.acl/src/main/java/com/diamondq/common/security/acl/api/SecurityContext.java
@@ -0,0 +1,8 @@
+package com.diamondq.common.security.acl.api;
+
+/**
+ * Describes the security context that this caller/callee is working in.
+ */
+public interface SecurityContext {
+
+}
---
+++ security/common-security.acl/src/main/java/com/diamondq/common/security/acl/model/AllAccessSecurityContext.java
@@ -0,0 +1,9 @@
+package com.diamondq.common.security.acl.model;
+
+import com.diamondq.common.security.acl.api.SecurityContext;
+
+public class AllAccessSecurityContext implements SecurityContext {
+
+	public AllAccessSecurityContext() {
+	}
+}
