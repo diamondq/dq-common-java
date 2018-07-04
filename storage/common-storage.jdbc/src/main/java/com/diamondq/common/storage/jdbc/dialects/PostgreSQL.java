a93c5ef8bae72e887247711f501155c7129b5f3f
Mike Mansell <me@michaelmansell.com>
2018-07-03 18:07:15 -0700

    Added offset to the list of reserved words for Postresql

--- storage/common-storage.jdbc/src/main/java/com/diamondq/common/storage/jdbc/dialects/PostgreSQL.java
+++ storage/common-storage.jdbc/src/main/java/com/diamondq/common/storage/jdbc/dialects/PostgreSQL.java
@@ -25,7 +25,7 @@ public class PostgreSQL extends AbstractDialect {
 
 	static {
 		String wordsStr =
-			"abort,acl,add,aggregate,append,archive,arch_store,backward,binary,boolean,change,cluster,copy,database,delimiter,delimiters,do,extend,explain,forward,heavy,index,inherits,isnull,light,listen,load,merge,nothing,notify,notnull,oids,purge,rename,replace,retrieve,returns,rule,recipe,setof,stdin,stdout,store,vacuum,verbose,version";
+			"abort,acl,add,aggregate,append,archive,arch_store,backward,binary,boolean,change,cluster,copy,database,delimiter,delimiters,do,extend,explain,forward,heavy,index,inherits,isnull,light,listen,load,merge,nothing,notify,notnull,offset,oids,purge,rename,replace,retrieve,returns,rule,recipe,setof,stdin,stdout,store,vacuum,verbose,version";
 		@NonNull
 		String[] words = wordsStr.split(",");
 		ImmutableSet.Builder<String> builder = ImmutableSet.builder();
