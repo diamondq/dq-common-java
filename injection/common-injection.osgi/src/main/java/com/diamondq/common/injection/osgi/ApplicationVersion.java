4f02a2fa9febfea9652c00b9572d06fc0c17c044
Mike Mansell <me@michaelmansell.com>
2018-08-24 08:20:51 -0700

    Added a class to carry the ApplicationVersion

---
+++ injection/common-injection.osgi/src/main/java/com/diamondq/common/injection/osgi/ApplicationVersion.java
@@ -0,0 +1,23 @@
+package com.diamondq.common.injection.osgi;
+
+import com.diamondq.common.utils.parsing.properties.PropertiesParsing;
+
+import java.util.Map;
+
+public class ApplicationVersion {
+
+	private String mVersion;
+
+	@SuppressWarnings("null")
+	public ApplicationVersion() {
+	}
+
+	public void onActivate(Map<String, Object> pProps) {
+
+		mVersion = PropertiesParsing.getNonNullString(pProps, ".application-version", "0.1.0-FALLBACK");
+	}
+
+	public String getVersion() {
+		return mVersion;
+	}
+}
