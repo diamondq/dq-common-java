2cced3a7639901e14c51acdf3dc0a177e8a67041
Mike Mansell <me@michaelmansell.com>
2018-09-17 14:24:20 -0700

    Added a setup routine for non-OSGi users of the ContextFactory

---
+++ utils/common-utils.context/src/main/java/com/diamondq/common/utils/context/impl/ContextFactorySetup.java
@@ -0,0 +1,34 @@
+package com.diamondq.common.utils.context.impl;
+
+import com.diamondq.common.utils.context.ContextFactory;
+import com.diamondq.common.utils.context.logging.LoggingContextHandler;
+
+/**
+ * This class is used for non-OSGi environments to get a ContextFactory
+ */
+public class ContextFactorySetup {
+
+  private static boolean setup = false;
+
+  public static ContextFactory setup() {
+    synchronized (ContextFactorySetup.class) {
+      if (setup == false) {
+
+        /* Create a new instance */
+
+        ContextFactoryImpl impl = new ContextFactoryImpl();
+
+        /* Register the basic handlers */
+
+        impl.addContextHandler(new LoggingContextHandler());
+
+        /* Activate it */
+
+        impl.onActivate();
+        setup = true;
+      }
+      return ContextFactory.getInstance();
+    }
+  }
+
+}
