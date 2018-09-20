def6a1c9b36dc417f9f75cc03089da35cffa70de
Mike Mansell <me@michaelmansell.com>
2018-09-19 21:19:24 -0700

    Modified the Verify functions to return the object (improves usefulness)

--- utils/common-utils.misc/src/main/java/com/diamondq/common/utils/misc/errors/Verify.java
+++ utils/common-utils.misc/src/main/java/com/diamondq/common/utils/misc/errors/Verify.java
@@ -2,20 +2,22 @@ package com.diamondq.common.utils.misc.errors;
 
 import com.diamondq.common.utils.misc.internationalization.MessagesEnum;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class Verify {
 
-  public static void notNullArg(@Nullable Object pObj, MessagesEnum pMessage, @Nullable Object @Nullable... pArgs) {
+  public static <T> @NonNull T notNullArg(@Nullable T pObj, MessagesEnum pMessage,
+    @Nullable Object @Nullable... pArgs) {
     if (pObj != null)
-      return;
+      return pObj;
 
     throw new ExtendedIllegalArgumentException(pMessage, pArgs);
   }
 
-  public static void notEmptyArg(@Nullable String pObj, MessagesEnum pMessage, @Nullable Object @Nullable... pArgs) {
+  public static String notEmptyArg(@Nullable String pObj, MessagesEnum pMessage, @Nullable Object @Nullable... pArgs) {
     if ((pObj != null) && (pObj.isEmpty() == false))
-      return;
+      return pObj;
 
     throw new ExtendedIllegalArgumentException(pMessage, pArgs);
   }
