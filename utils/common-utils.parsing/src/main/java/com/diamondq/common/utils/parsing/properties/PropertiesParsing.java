50b6951ac4866c2ef44dfac554ce74175e3b60e4
Mike Mansell <me@michaelmansell.com>
2018-11-18 10:58:41 -0800

    Added dictionary support for property parsing

--- utils/common-utils.parsing/src/main/java/com/diamondq/common/utils/parsing/properties/PropertiesParsing.java
+++ utils/common-utils.parsing/src/main/java/com/diamondq/common/utils/parsing/properties/PropertiesParsing.java
@@ -1,5 +1,6 @@
 package com.diamondq.common.utils.parsing.properties;
 
+import java.util.Dictionary;
 import java.util.Map;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -8,6 +9,16 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public class PropertiesParsing {
 
   public static boolean getNonNullBoolean(Map<String, Object> pProps, String pKey, Boolean pDefault) {
+    Object propObj = pProps.get(pKey);
+    Boolean bool;
+    if (propObj == null)
+      bool = pDefault;
+    else
+      bool = Boolean.parseBoolean(propObj.toString());
+    return bool;
+  }
+
+  public static boolean getNonNullBoolean(Dictionary<String, Object> pProps, String pKey, Boolean pDefault) {
     Object propObj = pProps.get(pKey);
     Boolean bool;
     if (propObj == null)
@@ -28,7 +39,28 @@ public class PropertiesParsing {
     return bool;
   }
 
+  public static @Nullable Boolean getNullableBoolean(Dictionary<String, Object> pProps, String pKey) {
+    Object propObj = pProps.get(pKey);
+    @Nullable
+    Boolean bool;
+    if (propObj == null)
+      bool = null;
+    else
+      bool = Boolean.parseBoolean(propObj.toString());
+    return bool;
+  }
+
   public static String getNonNullString(Map<String, Object> pProps, String pKey, String pDefault) {
+    Object propObj = pProps.get(pKey);
+    String val;
+    if (propObj == null)
+      val = pDefault;
+    else
+      val = propObj.toString();
+    return val;
+  }
+
+  public static String getNonNullString(Dictionary<String, Object> pProps, String pKey, String pDefault) {
     Object propObj = pProps.get(pKey);
     String val;
     if (propObj == null)
@@ -49,7 +81,28 @@ public class PropertiesParsing {
     return val;
   }
 
+  public static @Nullable String getNullableString(Dictionary<String, Object> pProps, String pKey) {
+    Object propObj = pProps.get(pKey);
+    @Nullable
+    String val;
+    if (propObj == null)
+      val = null;
+    else
+      val = propObj.toString();
+    return val;
+  }
+
   public static int getNonNullInt(Map<String, Object> pProps, String pKey, int pDefault) {
+    Object propObj = pProps.get(pKey);
+    int val;
+    if (propObj == null)
+      val = pDefault;
+    else
+      val = Integer.parseInt(propObj.toString());
+    return val;
+  }
+
+  public static int getNonNullInt(Dictionary<String, Object> pProps, String pKey, int pDefault) {
     Object propObj = pProps.get(pKey);
     int val;
     if (propObj == null)
@@ -70,7 +123,28 @@ public class PropertiesParsing {
     return val;
   }
 
+  public static @Nullable Integer getNullableInt(Dictionary<String, Object> pProps, String pKey) {
+    Object propObj = pProps.get(pKey);
+    @Nullable
+    Integer val;
+    if (propObj == null)
+      val = null;
+    else
+      val = Integer.parseInt(propObj.toString());
+    return val;
+  }
+
   public static long getNonNullLong(Map<String, Object> pProps, String pKey, long pDefault) {
+    Object propObj = pProps.get(pKey);
+    long val;
+    if (propObj == null)
+      val = pDefault;
+    else
+      val = Long.parseLong(propObj.toString());
+    return val;
+  }
+
+  public static long getNonNullLong(Dictionary<String, Object> pProps, String pKey, long pDefault) {
     Object propObj = pProps.get(pKey);
     long val;
     if (propObj == null)
@@ -91,6 +165,17 @@ public class PropertiesParsing {
     return val;
   }
 
+  public static @Nullable Long getNullableLong(Dictionary<String, Object> pProps, String pKey) {
+    Object propObj = pProps.get(pKey);
+    @Nullable
+    Long val;
+    if (propObj == null)
+      val = null;
+    else
+      val = Long.parseLong(propObj.toString());
+    return val;
+  }
+
   public static @NonNull String[] getStringArray(Map<String, Object> pProps, String pKey) {
     Object propObj = pProps.get(pKey);
     if (propObj == null)
@@ -101,7 +186,22 @@ public class PropertiesParsing {
     return results;
   }
 
+  public static @NonNull String[] getStringArray(Dictionary<String, Object> pProps, String pKey) {
+    Object propObj = pProps.get(pKey);
+    if (propObj == null)
+      return new String[0];
+    String val = propObj.toString();
+    @NonNull
+    String[] results = val.split(",");
+    return results;
+  }
+
   public static void removeAll(Map<String, Object> pProps, String... pKeys) {
+    for (String key : pKeys)
+      pProps.remove(key);
+  }
+
+  public static void removeAll(Dictionary<String, Object> pProps, String... pKeys) {
     for (String key : pKeys)
       pProps.remove(key);
   }
