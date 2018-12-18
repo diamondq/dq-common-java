c54792236c034bfaa9de23ac23be6c0aaf2d9b1b
Mike Mansell <me@michaelmansell.com>
2018-12-17 18:20:49 -0800

    Added a missing dependency

--- security/common-security.acl/src/main/java/com/diamondq/common/security/acl/impl/SecurityContextManagerImpl.java
+++ security/common-security.acl/src/main/java/com/diamondq/common/security/acl/impl/SecurityContextManagerImpl.java
@@ -4,7 +4,9 @@ import com.diamondq.common.security.acl.ACLMessages;
 import com.diamondq.common.security.acl.api.SecurityContext;
 import com.diamondq.common.security.acl.api.SecurityContextManager;
 import com.diamondq.common.security.acl.spi.SecurityContextSerializer;
+import com.diamondq.common.utils.context.Context;
 import com.diamondq.common.utils.context.ContextFactory;
+import com.diamondq.common.utils.misc.MiscMessages;
 import com.diamondq.common.utils.misc.errors.ExtendedIllegalStateException;
 import com.diamondq.common.utils.misc.errors.Verify;
 
@@ -16,9 +18,23 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.osgi.framework.Constants;
+import org.osgi.service.component.ComponentContext;
+
 public class SecurityContextManagerImpl implements SecurityContextManager {
 
+  private ContextFactory                                   mContextFactory;
+
   private ConcurrentMap<String, SecurityContextSerializer> mSerializers = new ConcurrentHashMap<>();
+
+  @SuppressWarnings("null")
+  public SecurityContextManagerImpl() {
+  }
+
+  public void setContextFactory(ContextFactory pContextFactory) {
+    ContextFactory.staticReportTrace(SecurityContextManagerImpl.class, this, pContextFactory);
+    mContextFactory = pContextFactory;
+  }
 
   public void addSecurityContextSerializer(SecurityContextSerializer pSerializer) {
     ContextFactory.staticReportTrace(SecurityContextManagerImpl.class, this, pSerializer);
@@ -30,78 +46,87 @@ public class SecurityContextManagerImpl implements SecurityContextManager {
     mSerializers.remove(pSerializer.getSerializerId(), pSerializer);
   }
 
+  public void onActivate(ComponentContext pContext) {
+    String pid = Verify.notNull((String) pContext.getProperties().get(Constants.SERVICE_PID));
+    Verify.notNullArg(mContextFactory, MiscMessages.VERIFY_DEPENDENCY_MISSING, "contextFactory", pid);
+  }
+
   /**
    * @see com.diamondq.common.security.acl.api.SecurityContextManager#serialize(com.diamondq.common.security.acl.api.SecurityContext)
    */
   @Override
   public byte[] serialize(SecurityContext pContext) {
-    LinkedHashMap<String, byte[]> byteMap = new LinkedHashMap<>();
-    for (SecurityContextSerializer ser : mSerializers.values()) {
-      byte[] bytes = ser.serialize(pContext);
-      if (bytes != null)
-        byteMap.put(ser.getSerializerId(), bytes);
-    }
-    if (byteMap.isEmpty() == true)
-      throw new ExtendedIllegalStateException(ACLMessages.NO_SUCH_SERIALIZER, pContext.getClass().getName());
-    try {
-      int byteCount = 1;
-      for (Map.Entry<String, byte[]> pair : byteMap.entrySet()) {
-        byteCount = byteCount + 2 + pair.getKey().getBytes("UTF-8").length + 2 + pair.getValue().length;
+    try (Context ctx = mContextFactory.newContext(SecurityContextManagerImpl.class, this, pContext)) {
+      LinkedHashMap<String, byte[]> byteMap = new LinkedHashMap<>();
+      for (SecurityContextSerializer ser : mSerializers.values()) {
+        byte[] bytes = ser.serialize(pContext);
+        if (bytes != null)
+          byteMap.put(ser.getSerializerId(), bytes);
       }
-      ByteBuffer buffer = ByteBuffer.allocate(byteCount);
-      buffer = buffer.order(ByteOrder.LITTLE_ENDIAN);
-      buffer.put((byte) byteMap.size());
-      for (Map.Entry<String, byte[]> pair : byteMap.entrySet()) {
-        byte[] data = pair.getKey().getBytes("UTF-8");
-        int dataLen = data.length;
-        byte[] bytes = pair.getValue();
-        int bytesLen = bytes.length;
-        /* Data Len */
-        buffer.putShort((short) dataLen);
-        /* Data */
-        buffer.put(data);
-        /* Bytes Len */
-        buffer.putShort((short) bytesLen);
-        /* Bytes */
-        buffer.put(bytes);
+      if (byteMap.isEmpty() == true)
+        throw new ExtendedIllegalStateException(ACLMessages.NO_SUCH_SERIALIZER, pContext.getClass().getName());
+      try {
+        int byteCount = 1;
+        for (Map.Entry<String, byte[]> pair : byteMap.entrySet()) {
+          byteCount = byteCount + 2 + pair.getKey().getBytes("UTF-8").length + 2 + pair.getValue().length;
+        }
+        ByteBuffer buffer = ByteBuffer.allocate(byteCount);
+        buffer = buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.put((byte) byteMap.size());
+        for (Map.Entry<String, byte[]> pair : byteMap.entrySet()) {
+          byte[] data = pair.getKey().getBytes("UTF-8");
+          int dataLen = data.length;
+          byte[] bytes = pair.getValue();
+          int bytesLen = bytes.length;
+          /* Data Len */
+          buffer.putShort((short) dataLen);
+          /* Data */
+          buffer.put(data);
+          /* Bytes Len */
+          buffer.putShort((short) bytesLen);
+          /* Bytes */
+          buffer.put(bytes);
+        }
+        return buffer.array();
       }
-      return buffer.array();
-    }
-    catch (UnsupportedEncodingException ex) {
-      throw new RuntimeException(ex);
+      catch (UnsupportedEncodingException ex) {
+        throw new RuntimeException(ex);
+      }
     }
   }
 
   @Override
   public SecurityContext deserialize(byte[] pBytes) {
-    ByteBuffer buffer = ByteBuffer.wrap(pBytes);
-    buffer = buffer.order(ByteOrder.LITTLE_ENDIAN);
-    int mapCount = buffer.get();
-    SecurityContext context = null;
-    for (int i = 0; i < mapCount; i++) {
-      /* Key Len */
-      short keyLen = buffer.getShort();
-      /* Key */
-      byte[] keyBytes = new byte[keyLen];
-      buffer.get(keyBytes);
-      String keyId;
-      try {
-        keyId = new String(keyBytes, "UTF-8");
+    try (Context ctx = mContextFactory.newContext(SecurityContextManagerImpl.class, this)) {
+      ByteBuffer buffer = ByteBuffer.wrap(pBytes);
+      buffer = buffer.order(ByteOrder.LITTLE_ENDIAN);
+      int mapCount = buffer.get();
+      SecurityContext context = null;
+      for (int i = 0; i < mapCount; i++) {
+        /* Key Len */
+        short keyLen = buffer.getShort();
+        /* Key */
+        byte[] keyBytes = new byte[keyLen];
+        buffer.get(keyBytes);
+        String keyId;
+        try {
+          keyId = new String(keyBytes, "UTF-8");
+        }
+        catch (UnsupportedEncodingException ex) {
+          throw new RuntimeException(ex);
+        }
+        /* Data Len */
+        short dataLen = buffer.getShort();
+        /* Data */
+        byte[] dataBytes = new byte[dataLen];
+        buffer.get(dataBytes);
+        SecurityContextSerializer serializer = mSerializers.get(keyId);
+        if (serializer == null)
+          throw new ExtendedIllegalStateException(ACLMessages.NO_SUCH_SERIALIZER, keyId);
+        context = serializer.deserialize(context, dataBytes);
       }
-      catch (UnsupportedEncodingException ex) {
-        throw new RuntimeException(ex);
-      }
-      /* Data Len */
-      short dataLen = buffer.getShort();
-      /* Data */
-      byte[] dataBytes = new byte[dataLen];
-      buffer.get(dataBytes);
-      SecurityContextSerializer serializer = mSerializers.get(keyId);
-      if (serializer == null)
-        throw new ExtendedIllegalStateException(ACLMessages.NO_SUCH_SERIALIZER, keyId);
-      context = serializer.deserialize(context, dataBytes);
+      return Verify.notNull(context);
     }
-    return Verify.notNull(context);
   }
 
 }
