292c972d1aa4892d68dc68b47994240bd174b3df
Mike Mansell <me@michaelmansell.com>
2019-06-30 23:56:19 -0700

    Added an explicit name for the DiamondQ ExecutorServices

--- injection/common-injection.micronaut/src/main/java/com/diamondq/common/injection/micronaut/ExecutorServiceFactory.java
+++ injection/common-injection.micronaut/src/main/java/com/diamondq/common/injection/micronaut/ExecutorServiceFactory.java
@@ -4,17 +4,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import io.micronaut.context.annotation.Factory;
 
 @Factory
 public class ExecutorServiceFactory {
-  public @Singleton ExecutorService getExecutorService() {
+  public @Named("DiamondQ") @Singleton ExecutorService getExecutorService() {
     return Executors.newCachedThreadPool();
   }
 
-  public @Singleton ScheduledExecutorService getScheduledExecutorService() {
+  public @Named("DiamondQ") @Singleton ScheduledExecutorService getScheduledExecutorService() {
     return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors() * 2);
   }
 }
