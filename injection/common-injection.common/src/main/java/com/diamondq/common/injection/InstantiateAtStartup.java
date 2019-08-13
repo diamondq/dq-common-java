666c7ac90ef7ca1193b1192aa59718f5df606ced
Mike Mansell <me@michaelmansell.com>
2019-08-12 17:21:59 -0700

    Removed the unused InstantiateAtStartup interface

--- injection/common-injection.common/src/main/java/com/diamondq/common/injection/InstantiateAtStartup.java
+++
@@ -1,9 +0,0 @@
-package com.diamondq.common.injection;
-
-/**
- * This marker interface is added to @Singleton's that should be started on startup instead of on-demand. This is used
- * as a replacement for the injection platform specific model.
- */
-public interface InstantiateAtStartup {
-
-}
