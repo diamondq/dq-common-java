e57b2e75e45289fd2dbfd7fe21062b91c392332b
Mike Mansell <me@michaelmansell.com>
2020-05-02 21:00:30 -0700

    Added support for InjectionContext and CDI

---
+++ injection/common-injection.cdi/src/main/java/com/diamondq/common/injection/impl/InjectionContextImpl.java
@@ -0,0 +1,77 @@
+package com.diamondq.common.injection.impl;
+
+import com.diamondq.common.injection.InjectionContext;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.literal.NamedLiteral;
+import javax.enterprise.inject.se.SeContainer;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class InjectionContextImpl implements InjectionContext {
+
+  private @Nullable SeContainer mAppContext;
+
+  public InjectionContextImpl() {
+  }
+
+  public void setApplicationContext(SeContainer pAppContext) {
+    mAppContext = pAppContext;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionContext#findBean(java.lang.Class, java.lang.String)
+   */
+  @Override
+  public <T> Optional<T> findBean(Class<T> pBeanType, @Nullable String pName) {
+    Instance<T> instance;
+    if (pName != null)
+      instance = Objects.requireNonNull(mAppContext).select(pBeanType, NamedLiteral.of(pName));
+    else
+      instance = Objects.requireNonNull(mAppContext).select(pBeanType);
+    if (instance.isResolvable() == false)
+      return Optional.empty();
+    return Optional.of(instance.get());
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionContext#getBeansOfType(java.lang.Class, java.lang.String)
+   */
+  @Override
+  public <T> Collection<T> getBeansOfType(Class<T> pBeanType, @Nullable String pName) {
+    List<T> list = new ArrayList<>();
+    Instance<T> instance;
+    if (pName != null)
+      instance = Objects.requireNonNull(mAppContext).select(pBeanType, NamedLiteral.of(pName));
+    else
+      instance = Objects.requireNonNull(mAppContext).select(pBeanType);
+    for (Iterator<T> i = instance.iterator(); i.hasNext();)
+      list.add(i.next());
+    return list;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionContext#getProperties(java.lang.String)
+   */
+  @Override
+  public Map<String, Object> getProperties(String pPrefix) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * @see java.io.Closeable#close()
+   */
+  @Override
+  public void close() throws IOException {
+    Objects.requireNonNull(mAppContext).close();
+  }
+}
---
+++ injection/common-injection.cdi/src/main/java/com/diamondq/common/injection/impl/InjectionStartupBuilderImpl.java
@@ -0,0 +1,142 @@
+package com.diamondq.common.injection.impl;
+
+import com.diamondq.common.injection.InjectionContext;
+import com.diamondq.common.injection.InjectionStartupBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.util.AnnotationLiteral;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class InjectionStartupBuilderImpl implements InjectionStartupBuilder {
+
+  private final Set<String>               mEnvironmentTags;
+
+  private final List<Map<String, Object>> mPropertiesList;
+
+  private final Set<Object>               mSingletons;
+
+  private final Map<Object, String>       mNamedSingletons;
+
+  public InjectionStartupBuilderImpl() {
+    mEnvironmentTags = new HashSet<>();
+    mPropertiesList = new ArrayList<Map<String, Object>>();
+    mSingletons = new HashSet<>();
+    mNamedSingletons = new HashMap<>();
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionStartupBuilder#addEnvironmentTag(java.lang.String)
+   */
+  @Override
+  public InjectionStartupBuilder addEnvironmentTag(String pTag) {
+    synchronized (this) {
+      mEnvironmentTags.add(pTag);
+    }
+    return this;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionStartupBuilder#addPropertyMap(java.util.Map)
+   */
+  @Override
+  public InjectionStartupBuilder addPropertyMap(Map<String, Object> pProperties) {
+    synchronized (this) {
+      final Map<String, Object> copy = new HashMap<>(pProperties);
+      mPropertiesList.add(copy);
+    }
+    return this;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionStartupBuilder#singletons(java.lang.Object[])
+   */
+  @Override
+  public InjectionStartupBuilder singletons(@NonNull Object @NonNull... pSingletons) {
+    synchronized (this) {
+      for (final Object o : pSingletons)
+        mSingletons.add(o);
+    }
+    return this;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionStartupBuilder#singleton(java.lang.Object, java.lang.String)
+   */
+  @Override
+  public InjectionStartupBuilder singleton(Object pSingleton, String pName) {
+    synchronized (this) {
+      mNamedSingletons.put(pSingleton, pName);
+    }
+    return this;
+  }
+
+  /**
+   * @see com.diamondq.common.injection.InjectionStartupBuilder#buildAndStart()
+   */
+  @Override
+  public InjectionContext buildAndStart() {
+    synchronized (this) {
+      SeContainerInitializer initializer = SeContainerInitializer.newInstance();
+      Map<String, Object> allProps = new HashMap<>();
+      for (final Map<String, Object> props : mPropertiesList)
+        allProps.putAll(props);
+      initializer.setProperties(allProps);
+      final InjectionContextImpl injectionContext = new InjectionContextImpl();
+      initializer.addExtensions(new Extension() {
+        @SuppressWarnings("unused")
+        void afterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager beanManager) {
+          event.addBean().types(InjectionContext.class).qualifiers(new AnnotationLiteral<Default>() {
+            private static final long serialVersionUID = 1L;
+          }, new AnnotationLiteral<Any>() {
+            private static final long serialVersionUID = 1L;
+          }).scope(ApplicationScoped.class).name(InjectionContext.class.getName()).beanClass(InjectionContext.class)
+            .createWith(creationalContext -> {
+              return injectionContext;
+            });
+          for (Object singleton : mSingletons) {
+            event.addBean().types(singleton.getClass()).qualifiers(new AnnotationLiteral<Default>() {
+              private static final long serialVersionUID = 1L;
+            }, new AnnotationLiteral<Any>() {
+              private static final long serialVersionUID = 1L;
+            }).scope(ApplicationScoped.class).name(singleton.getClass().getName()).beanClass(singleton.getClass())
+              .createWith(creationalContext -> {
+                return singleton;
+              });
+          }
+          for (Map.Entry<Object, String> pair : mNamedSingletons.entrySet()) {
+            Object singleton = pair.getKey();
+            String name = pair.getValue();
+            event.addBean().types(singleton.getClass()).qualifiers(new AnnotationLiteral<Default>() {
+              private static final long serialVersionUID = 1L;
+            }, new AnnotationLiteral<Any>() {
+              private static final long serialVersionUID = 1L;
+            }).scope(ApplicationScoped.class).name(name).beanClass(singleton.getClass())
+              .createWith(creationalContext -> {
+                return singleton;
+              });
+          }
+        }
+      });
+      SeContainer container = initializer.initialize();
+      injectionContext.setApplicationContext(container);
+      return injectionContext;
+    }
+  }
+
+}
