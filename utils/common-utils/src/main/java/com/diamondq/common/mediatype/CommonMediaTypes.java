2c7677830dff3adb26f81ff96d80bf1eed038933
Mike Mansell <me@michaelmansell.com>
2020-06-29 15:25:16 -0700

    Added some basic MediaType support

---
+++ utils/common-utils/src/main/java/com/diamondq/common/mediatype/CommonMediaTypes.java
@@ -0,0 +1,113 @@
+package com.diamondq.common.mediatype;
+
+import com.google.common.net.MediaType;
+
+public interface CommonMediaTypes {
+
+  public static final MediaType APPLICATION_ATOM_XML        = MediaType.create("application", "atom+xml");
+
+  public static final MediaType APPLICATION_BZIP2           = MediaType.create("application", "x-bzip2");
+
+  public static final MediaType APPLICATION_GRAPHQL         = MediaType.create("application", "graphql");
+
+  public static final MediaType APPLICATION_GZIP            = MediaType.create("application", "x-gzip");
+
+  public static final MediaType APPLICATION_HAL_JSON        = MediaType.create("application", "hal+json");
+
+  public static final MediaType APPLICATION_HAL_XML         = MediaType.create("application", "hal+xml");
+
+  public static final MediaType APPLICATION_JAVASCRIPT      = MediaType.create("application", "javascript");
+
+  public static final MediaType APPLICATION_JOSE            = MediaType.create("application", "jose");
+
+  public static final MediaType APPLICATION_JOSE_JSON       = MediaType.create("application", "jose+json");
+
+  public static final MediaType APPLICATION_JSON            = MediaType.create("application", "json");
+
+  public static final MediaType APPLICATION_PDF             = MediaType.create("application", "pdf");
+
+  public static final MediaType APPLICATION_OCTET_STREAM    = MediaType.create("application", "octet-stream");
+
+  public static final MediaType APPLICATION_VND_ERROR       = MediaType.create("application", "vnd.error+json");
+
+  public static final MediaType APPLICATION_JSON_STREAM     = MediaType.create("application", "x-json-stream");
+
+  public static final MediaType APPLICATION_FORM_URLENCODED = MediaType.create("application", "x-www-form-urlencoded");
+
+  public static final MediaType APPLICATION_YAML            = MediaType.create("application", "x-yaml");
+
+  public static final MediaType APPLICATION_XHTML           = MediaType.create("application", "xhtml+xml");
+
+  public static final MediaType APPLICATION_XML             = MediaType.create("application", "xml");
+
+  public static final MediaType APPLICATION_ZIP             = MediaType.create("application", "zip");
+
+  public static final MediaType AUDIO_BASIC                 = MediaType.create("audio", "basic");
+
+  public static final MediaType AUDIO_MP4                   = MediaType.create("audio", "mp4");
+
+  public static final MediaType AUDIO_MPEG                  = MediaType.create("audio", "mpeg");
+
+  public static final MediaType AUDIO_OGG                   = MediaType.create("audio", "ogg");
+
+  public static final MediaType AUDIO_WEBM                  = MediaType.create("audio", "webm");
+
+  public static final MediaType MULTIPART_FORM_DATA         = MediaType.create("multipart", "form-data");
+
+  public static final MediaType TEXT_CALENDAR               = MediaType.create("text", "calendar");
+
+  public static final MediaType TEXT_CSS                    = MediaType.create("text", "css");
+
+  public static final MediaType TEXT_CSV                    = MediaType.create("text", "csv");
+
+  public static final MediaType TEXT_EVENT_STREAM           = MediaType.create("text", "event-stream");
+
+  public static final MediaType TEXT_HTML                   = MediaType.create("text", "html");
+
+  public static final MediaType TEXT_JAVASCRIPT             = MediaType.create("text", "javascript");
+
+  public static final MediaType TEXT_JSON                   = MediaType.create("text", "json");
+
+  public static final MediaType TEXT_PLAIN                  = MediaType.create("text", "plain");
+
+  public static final MediaType TEXT_TSV                    = MediaType.create("text", "tab-separated-values");
+
+  public static final MediaType TEXT_VCARD                  = MediaType.create("text", "vcard");
+
+  public static final MediaType TEXT_XML                    = MediaType.create("text", "xml");
+
+  public static final MediaType IMAGE_BMP                   = MediaType.create("image", "bmp");
+
+  public static final MediaType IMAGE_GIF                   = MediaType.create("image", "gif");
+
+  public static final MediaType IMAGE_ICO                   = MediaType.create("image", "vnd.microsoft.icon");
+
+  public static final MediaType IMAGE_JPEG                  = MediaType.create("image", "jpeg");
+
+  public static final MediaType IMAGE_PSD                   = MediaType.create("image", "vnd.adobe.photoshop");
+
+  public static final MediaType IMAGE_PNG                   = MediaType.create("image", "png");
+
+  public static final MediaType IMAGE_SVG                   = MediaType.create("image", "svg+xml");
+
+  public static final MediaType IMAGE_TIFF                  = MediaType.create("image", "tiff");
+
+  public static final MediaType IMAGE_WEBP                  = MediaType.create("image", "webp");
+
+  public static final MediaType VIDEO_MP4                   = MediaType.create("video", "mp4");
+
+  public static final MediaType VIDEO_MPEG                  = MediaType.create("video", "mpeg");
+
+  public static final MediaType VIDEO_OGG                   = MediaType.create("video", "ogg");
+
+  public static final MediaType VIDEO_QUICKTIME             = MediaType.create("video", "quicktime");
+
+  public static final MediaType VIDEO_WEBM                  = MediaType.create("video", "webm");
+
+  public static final MediaType VIDEO_WMV                   = MediaType.create("video", "x-ms-wmv");
+
+  public static final MediaType VIDEO_FLV                   = MediaType.create("video", "x-flv");
+
+  public static final MediaType VIDEO_THREE_GPP             = MediaType.create("video", "3gpp");
+
+}
---
+++ utils/common-utils/src/main/java/com/diamondq/common/mediatype/MediaTypeUtils.java
@@ -0,0 +1,8 @@
+package com.diamondq.common.mediatype;
+
+public abstract class MediaTypeUtils {
+
+  private MediaTypeUtils() {
+  }
+
+}
