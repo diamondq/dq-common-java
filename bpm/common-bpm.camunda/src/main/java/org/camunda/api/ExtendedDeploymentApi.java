bc20b56bde06d0c2cf21be8401ad2967abc23650
Mike Mansell <mike.mansell@rdffg.bc.ca>
2021-02-28 18:53:41 -0800

    Missed a case of a manual API for Camunda that didn't have the new AuthInfo header.

--- bpm/common-bpm.camunda/src/main/java/org/camunda/api/ExtendedDeploymentApi.java
+++ bpm/common-bpm.camunda/src/main/java/org/camunda/api/ExtendedDeploymentApi.java
@@ -9,11 +9,12 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.multipart.MultipartBody;
+import com.diamondq.common.injection.micronaut.auth.AuthInfo;
 
 @Client(id = "camunda", path = "/engine-rest/deployment")
 public interface ExtendedDeploymentApi {
 
   @Post(value = "/create", produces = MediaType.MULTIPART_FORM_DATA, consumes = MediaType.APPLICATION_JSON)
-  Optional<DeploymentWithDefinitionsDto> createDeployment(@Body MultipartBody body);
+  Optional<DeploymentWithDefinitionsDto> createDeployment(AuthInfo authInfo, @Body MultipartBody body);
 
 }
