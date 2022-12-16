9932db0a019f7415277bb4445e3e9c0e03037657
Mike Mansell <me@michaelmansell.com>
2022-12-16 08:34:29 -0800

    Upgraded the Camunda OpenAPI to the 7.17 version. This removed a lot of custom entries.

--- bpm/common-bpm.camunda/src/main/java/org/camunda/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Information about the hub.workflow.camunda package
- */
-@org.checkerframework.framework.qual.DefaultQualifier(value = org.checkerframework.checker.nullness.qual.NonNull.class)
-@org.eclipse.jdt.annotation.NonNullByDefault
-package org.camunda.model;
---
+++ bpm/common-bpm.camunda/src/openapi/READ_ME_CHANGES.txt
@@ -0,0 +1,2 @@
+A handful of operations from the /history section had to be removed because the Micronaut ASM annotation compiler
+was failing with "Method Too Large".
