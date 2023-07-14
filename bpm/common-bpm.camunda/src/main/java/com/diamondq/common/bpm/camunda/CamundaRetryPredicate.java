8663d531c13522d6dc8b3e395efd7afed21efd73
Mike Mansell <me@michaelmansell.com>
2023-07-14 14:04:39 -0700

    Added a basic @Retryable to Camunda APIs with a configurable predicate.

---
+++ bpm/common-bpm.camunda/src/main/java/com/diamondq/common/bpm/camunda/CamundaRetryPredicate.java
@@ -0,0 +1,22 @@
+package com.diamondq.common.bpm.camunda;
+
+import io.micronaut.retry.annotation.RetryPredicate;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class CamundaRetryPredicate implements RetryPredicate {
+
+  public static void setRetryPredicate(Function<Throwable, Boolean> pRetryPredicate) {
+    sRetryPredicate = pRetryPredicate;
+  }
+
+  private static @Nullable Function<Throwable, Boolean> sRetryPredicate = null;
+
+  @Override
+  public boolean test(Throwable pThrowable) {
+    @Nullable Function<Throwable, Boolean> localRetryPredicate = sRetryPredicate;
+    if (localRetryPredicate == null) return false;
+    return localRetryPredicate.apply(pThrowable);
+  }
+}
