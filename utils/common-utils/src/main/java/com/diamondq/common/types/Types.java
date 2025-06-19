b1ca13c0fc894b0153fd4fd1b53a0cd1ff481d46
Mike Mansell <mmansell@diamondq.com>
2025-06-19 10:05:04 -0700

    Added a few more missing common TypeReferences.

--- utils/common-utils/src/main/java/com/diamondq/common/types/Types.java
+++ utils/common-utils/src/main/java/com/diamondq/common/types/Types.java
@@ -4,12 +4,15 @@ import com.diamondq.common.TypeReference;
 import com.diamondq.common.i18n.I18NString;
 import org.jetbrains.annotations.Nullable;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -23,7 +26,7 @@ public abstract class Types {
   /**
    * Constant for string argument.
    */
-  public static final TypeReference<String> STRING = new TypeReference<String>() {
+  public static final TypeReference<String> STRING = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable String> NULLABLE_STRING = new TypeReference<@Nullable String>() {
@@ -32,7 +35,7 @@ public abstract class Types {
   /**
    * Constant for int argument.
    */
-  public static final TypeReference<Integer> INT = new TypeReference<Integer>() {
+  public static final TypeReference<Integer> INT = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Integer> NULLABLE_INT = new TypeReference<@Nullable Integer>() {
@@ -41,7 +44,7 @@ public abstract class Types {
   /**
    * Constant for long argument.
    */
-  public static final TypeReference<Long> LONG = new TypeReference<Long>() {
+  public static final TypeReference<Long> LONG = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Long> NULLABLE_LONG = new TypeReference<@Nullable Long>() {
@@ -50,7 +53,7 @@ public abstract class Types {
   /**
    * Constant for float argument.
    */
-  public static final TypeReference<Float> FLOAT = new TypeReference<Float>() {
+  public static final TypeReference<Float> FLOAT = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Float> NULLABLE_FLOAT = new TypeReference<@Nullable Float>() {
@@ -59,7 +62,7 @@ public abstract class Types {
   /**
    * Constant for double argument.
    */
-  public static final TypeReference<Double> DOUBLE = new TypeReference<Double>() {
+  public static final TypeReference<Double> DOUBLE = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Double> NULLABLE_DOUBLE = new TypeReference<@Nullable Double>() {
@@ -76,7 +79,7 @@ public abstract class Types {
   /**
    * Constant for byte argument.
    */
-  public static final TypeReference<Byte> BYTE = new TypeReference<Byte>() {
+  public static final TypeReference<Byte> BYTE = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Byte> NULLABLE_BYTE = new TypeReference<@Nullable Byte>() {
@@ -85,7 +88,7 @@ public abstract class Types {
   /**
    * Constant for boolean argument.
    */
-  public static final TypeReference<Boolean> BOOLEAN = new TypeReference<Boolean>() {
+  public static final TypeReference<Boolean> BOOLEAN = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Boolean> NULLABLE_BOOLEAN = new TypeReference<@Nullable Boolean>() {
@@ -94,7 +97,7 @@ public abstract class Types {
   /**
    * Constant char argument.
    */
-  public static final TypeReference<Character> CHAR = new TypeReference<Character>() {
+  public static final TypeReference<Character> CHAR = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Character> NULLABLE_CHAR = new TypeReference<@Nullable Character>() {
@@ -103,7 +106,7 @@ public abstract class Types {
   /**
    * Constant short argument.
    */
-  public static final TypeReference<Short> SHORT = new TypeReference<Short>() {
+  public static final TypeReference<Short> SHORT = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Short> NULLABLE_SHORT = new TypeReference<@Nullable Short>() {
@@ -112,19 +115,23 @@ public abstract class Types {
   /**
    * Default Object argument.
    */
-  public static final TypeReference<Object> OBJECT = new TypeReference<Object>() {
+  public static final TypeReference<Object> OBJECT = new TypeReference<>() {
   };
 
+  public static final TypeReference<Class<?>>         CLASS           = new TypeReference<>() {
+  };
+  public static final TypeReference<DataSource>       DATA_SOURCE     = new TypeReference<>() {
+  };
   public static final TypeReference<@Nullable Object> NULLABLE_OBJECT = new TypeReference<@Nullable Object>() {
   };
 
-  public static final TypeReference<Map<String, String>> MAP_OF_STRING_TO_STRING = new TypeReference<Map<String, String>>() {
+  public static final TypeReference<Map<String, String>> MAP_OF_STRING_TO_STRING = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Map<String, String>> NULLABLE_MAP_OF_STRING_TO_STRING = new TypeReference<@Nullable Map<String, String>>() {
   };
 
-  public static final TypeReference<Map<String, Long>> MAP_OF_STRING_TO_LONG = new TypeReference<Map<String, Long>>() {
+  public static final TypeReference<Map<String, Long>> MAP_OF_STRING_TO_LONG = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Map<String, Long>> NULLABLE_MAP_OF_STRING_TO_LONG = new TypeReference<@Nullable Map<String, Long>>() {
@@ -132,62 +139,68 @@ public abstract class Types {
 
   public static final TypeReference<Map<String, @Nullable Object>> MAP_OF_STRING_TO_NULLABLE_OBJECT = new TypeReference<Map<String, @Nullable Object>>() {
   };
-  public static final TypeReference<Map<String, Object>>           MAP_OF_STRING_TO_OBJECT          = new TypeReference<Map<String, Object>>() {
+  public static final TypeReference<Map<String, Object>>           MAP_OF_STRING_TO_OBJECT          = new TypeReference<>() {
   };
 
-  public static final TypeReference<List<String>> LIST_OF_STRING = new TypeReference<List<String>>() {
+  public static final TypeReference<List<String>> LIST_OF_STRING = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable List<String>> NULLABLE_LIST_OF_STRING = new TypeReference<@Nullable List<String>>() {
   };
+  public static final TypeReference<List<Object>>           LIST_OF_OBJECT          = new TypeReference<>() {
+  };
 
-  public static final TypeReference<Set<String>> SET_OF_STRING = new TypeReference<Set<String>>() {
+  public static final TypeReference<Set<String>> SET_OF_STRING = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Set<String>> NULLABLE_SET_OF_STRING = new TypeReference<@Nullable Set<String>>() {
   };
 
-  public static final TypeReference<Collection<String>> COLLECTION_OF_STRING = new TypeReference<Collection<String>>() {
+  public static final TypeReference<Collection<String>> COLLECTION_OF_STRING = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable Collection<String>> NULLABLE_COLLECTION_OF_STRING = new TypeReference<@Nullable Collection<String>>() {
   };
 
-  public static final TypeReference<List<Long>> LIST_OF_LONG = new TypeReference<List<Long>>() {
+  public static final TypeReference<List<Long>> LIST_OF_LONG = new TypeReference<>() {
   };
 
   public static final TypeReference<@Nullable List<Long>> NULLABLE_LIST_OF_LONG = new TypeReference<@Nullable List<Long>>() {
   };
 
-  public static final TypeReference<I18NString> I18NSTRING = new TypeReference<I18NString>() {
+  public static final TypeReference<I18NString> I18NSTRING = new TypeReference<>() {
   };
 
-  public static final TypeReference<Optional<?>> OPTIONAL_OF_WILD = new TypeReference<Optional<?>>() {
+  public static final TypeReference<Optional<?>> OPTIONAL_OF_WILD = new TypeReference<>() {
   };
 
-  public static final TypeReference<BigDecimal> BIG_DECIMAL = new TypeReference<BigDecimal>() {
+  public static final TypeReference<BigDecimal> BIG_DECIMAL = new TypeReference<>() {
+  };
+  public static final TypeReference<BigInteger> BIG_INTEGER = new TypeReference<>() {
   };
 
   /* Date/Time */
 
-  public static final TypeReference<LocalDate>      LOCAL_DATE       = new TypeReference<LocalDate>() {         // type
+  public static final TypeReference<Date>           DATE             = new TypeReference<>() {
   };
-  public static final TypeReference<LocalDateTime>  LOCAL_DATE_TIME  = new TypeReference<LocalDateTime>() {         // type
+  public static final TypeReference<LocalDate>      LOCAL_DATE       = new TypeReference<>() {         // type
   };
-  public static final TypeReference<ZonedDateTime>  ZONED_DATE_TIME  = new TypeReference<ZonedDateTime>() {
+  public static final TypeReference<LocalDateTime>  LOCAL_DATE_TIME  = new TypeReference<>() {         // type
   };
-  public static final TypeReference<OffsetDateTime> OFFSET_DATE_TIME = new TypeReference<OffsetDateTime>() {
+  public static final TypeReference<ZonedDateTime>  ZONED_DATE_TIME  = new TypeReference<>() {
+  };
+  public static final TypeReference<OffsetDateTime> OFFSET_DATE_TIME = new TypeReference<>() {
   };
 
 
   /* ARRAYS */
 
-  public static final TypeReference<String[]> ARRAY_OF_STRING = new TypeReference<String[]>() {
+  public static final TypeReference<String[]>     ARRAY_OF_STRING      = new TypeReference<>() {
   };
-
-  public static final TypeReference<Boolean[]> ARRAY_OF_BOOLEAN = new TypeReference<Boolean[]>() {         // type
+  public static final TypeReference<Boolean[]>    ARRAY_OF_BOOLEAN     = new TypeReference<>() {         // type
   };
-
-  public static final TypeReference<BigDecimal[]> ARRAY_OF_BIG_DECIMAL = new TypeReference<BigDecimal[]>() {         // type
+  public static final TypeReference<BigDecimal[]> ARRAY_OF_BIG_DECIMAL = new TypeReference<>() {         // type
+  };
+  public static final TypeReference<byte[]>       ARRAY_OF_BYTE        = new TypeReference<>() {
   };
 }
